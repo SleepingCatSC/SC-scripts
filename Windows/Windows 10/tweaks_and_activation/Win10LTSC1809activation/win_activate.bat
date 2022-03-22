68a6c95869c1b63633076bbecc84b679005cf048
Sleeping Cat <50832332+SleepingCat1@users.noreply.github.com>
2022-03-23 02:33:57 +0400

    Create win_activate.bat

---
+++ Windows/Windows 10/tweaks_and_activation/Win10LTSC1809activation/win_activate.bat
@@ -0,0 +1,17 @@
+if exist "%SystemRoot%\SysWOW64" path %path%;%windir%\SysNative;%SystemRoot%\SysWOW64;%~dp0
+bcdedit >nul
+if '%errorlevel%' NEQ '0' (goto UACPrompt) else (goto UACAdmin)
+:UACPrompt
+%1 start "" mshta vbscript:createobject("shell.application").shellexecute("""%~0""","::",,"runas",1)(window.close)&exit
+exit /B
+:UACAdmin
+cd /d "%~dp0"
+xcopy /e skus %windir%\system32\spp\tokens\skus
+cscript.exe %windir%\system32\slmgr.vbs /rilc
+cscript.exe %windir%\system32\slmgr.vbs /upk >nul 2>&1
+cscript.exe %windir%\system32\slmgr.vbs /ckms >nul 2>&1
+cscript.exe %windir%\system32\slmgr.vbs /cpky >nul 2>&1
+cscript.exe %windir%\system32\slmgr.vbs /ipk M7XTQ-FN8P6-TTKYV-9D4CC-J462D
+cscript.exe %windir%\system32\slmgr.vbs /skms kms.teevee.asia
+cscript.exe %windir%\system32\slmgr.vbs /ato
+taskkill /f /im explorer.exe & start explorer.exe
