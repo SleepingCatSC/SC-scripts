be57b846fe6f8e443351c5626e546372a37d8a4a
Sleeping Cat <50832332+SleepingCat1@users.noreply.github.com>
2022-04-20 01:11:29 +0400

    Uploaded updated xubuntu-tweaks

---
+++ Linux/Xubuntu/xubuntu-tweaks/tweak-xubuntu.sh
@@ -0,0 +1,24 @@
+xargs sudo apt -y purge <remove-packages;sudo apt -y autoremove
+sudo apt update;sudo apt -y upgrade
+xargs sudo apt -y install <install-packages
+wget https://github.com/clickot/ungoogled-chromium-binaries/releases/download/99.0.4844.84-1.1/ungoogled-chromium_99.0.4844.84-1.1.AppImage -P .
+git clone https://github.com/elfmz/far2l
+mkdir -p far2l/_build;cd far2l/_build
+cmake -DUSEWX=yes -DCMAKE_BUILD_TYPE=Release ..
+make -j$(nproc --all)
+sudo make install
+cd -;rm -rf far2l
+sudo swapoff -a
+sudo dd if=/dev/zero of=/swapfile bs=1024 count=16777216
+sudo mkswap /swapfile
+sudo chmod 600 /swapfile
+sudo swapon /swapfile
+sudo rm /usr/share/xfce4/backdrops/*
+sudo find /usr/share/backgrounds/ -name \* -type f -delete
+sudo wget https://www.htmlcsscolor.com/preview/32x32/000000.png -P /usr/share/xfce4/backdrops
+cp xfce-perchannel-xml/* ~/.config/xfce4/xfconf/xfce-perchannel-xml
+sudo mkdir /etc/X11/xorg.conf.d/;cat turn-off-mouse-acceleration | sudo tee -a /etc/X11/xorg.conf.d/50-mouse-acceleration.conf
+sudo cp locale /etc/default
+cat bash-aliases >> ~/.bashrc
+cat pac-proxy | sudo tee -a /etc/environment
+reboot
